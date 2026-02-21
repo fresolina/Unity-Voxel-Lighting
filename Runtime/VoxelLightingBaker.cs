3f002599d371bbbf4041f04a68ed85fdaa0a20ea
fresolina <74408325+fresolina@users.noreply.github.com>
2026-02-21 11:11:35 +0100

    Simplify baking process, support normal map

--- Runtime/VoxelLightingBaker.cs
+++ Runtime/VoxelLightingBaker.cs
@@ -61,47 +61,44 @@ namespace Lotec.Lighting {
         }
 #endif
 
-        public bool TryBake(out string error) {
+        public void Bake() {
             LightingVolume volume = _sdfShaderGlobals.volume;
+            string error;
             if (volume == null) {
-                error = "Target SdfVolume is not assigned.";
-                return false;
+                Debug.LogError("Target SdfVolume is not assigned.", _sdfShaderGlobals);
+                return;
             }
 
             // Bake SDF fields.
-            if (_sdfBaker.sdfBakeCompute == null) {
-                error = "SDF Bake Compute is not assigned to SdfBaker.";
-                return false;
-            }
             if (!_sdfBaker.TryBake(volume, volume.TrimmedMaxResolution, volume.BakeRoot.name, out Texture3D bakedSdf, out error)) {
-                return false;
+                Debug.LogError("SDF Bake failed: " + error, _sdfShaderGlobals);
+                return;
             }
             volume.sdfHiresTexture = bakedSdf;
             if (!_sdfBaker.TryBake(volume, volume.TrimmedMaxResolution / LowresDownscaleFactor, volume.BakeRoot.name + "_Lowres", out bakedSdf, out error)) {
-                return false;
+                Debug.LogError("SDF Bake failed: " + error, _sdfShaderGlobals);
+                return;
             }
             volume.sdfLowresTexture = bakedSdf;
 
             // Bake occlusion bitmask field. TODO: Make this optional.
             if (!_occlusionBitmaskBaker.TryBake(volume, out Texture3D bakedBitmask, out error)) {
-                return false;
+                return;
             }
             volume.occlusionBitmaskTexture = bakedBitmask;
 
             // Material baker produces two lower-res material textures (albedo+roughness, emission+metallic)
             if (_materialBaker.MaterialBakeCompute == null) {
                 error = "Material Bake Compute is not assigned to MaterialBaker.";
-                return false;
+                return;
             }
             string matErr = _materialBaker.Bake(volume, out Texture3D bakedAlbedoRoughness, out Texture3D bakedEmissionMetallic, LowresDownscaleFactor);
             if (!string.IsNullOrEmpty(matErr)) {
                 error = "MaterialBaker failed: " + matErr;
-                return false;
+                return;
             }
             volume.materialAlbedoRoughnessTexture = bakedAlbedoRoughness;
             volume.materialEmissionMetallicTexture = bakedEmissionMetallic;
-
-            return true;
         }
     }
 }
