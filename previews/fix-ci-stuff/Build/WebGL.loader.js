9be83ce5fb3a182af5003a92c58ad84c5d365b57
fresolina <fresolina@users.noreply.github.com>
2026-03-29 11:48:51 +0000

    deploy: 288f797c2355228cbc33411f3d68fb733663b471

--- previews/fix-ci-stuff/Build/WebGL.loader.js
+++ previews/fix-ci-stuff/Build/WebGL.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 49181762,
+    wasmFileSize: 49183623,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
